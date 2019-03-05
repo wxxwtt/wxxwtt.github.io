92989d301f00a56e9c1c544704469831a475b6b5
Tom <weixinxiu@digilinx.cn>
2019-03-05 15:15:03 +0800

    pwa

---
+++ precache-manifest.b25e585e03a43d51bc78f3ee1108d3b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d3a8d09733e250aad362",
+    "url": "/js/chunk-vendors.27a3e7ab.js"
+  },
+  {
+    "revision": "02e696d847b5271f8250",
+    "url": "/js/app.c5182453.js"
+  },
+  {
+    "revision": "48af24cd7dae7280ac56",
+    "url": "/js/about.41d636ee.js"
+  },
+  {
+    "revision": "b7e5f1266624f124960399550b87020f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "02e696d847b5271f8250",
+    "url": "/css/app.4383fdc6.css"
+  }
+];
